b0afdff01442203dc214d9552058cc79732fd5fc
IpotaZero <kiratoki512810@gmail.com>
2024-11-08 16:46:28 +0900

    escape

--- menu.js
+++ menu.js
@@ -1,17 +1,16 @@
-var html = '<div id="globalnavi">';
-html += '<ul id="mainMenu">';
-html += '<li id="nav-top-link"><a href="index.html"><i class="icon-top"></i>Top</a></li>';
-html += '<li><a href="info.html"><i class="icon-info"></i>Info</a></li>';
-html += '<li><a href="work.html"><i class="icon-work"></i>Work</a></li>';
-html += '<li><a href="member.html"><i class="icon-member"></i>Member</a></li>';
+var html = ''
+html += '<ul id="mainMenu">'
+html += '<li><a href="info.html"><i class="icon-info"></i>Info</a></li>'
+html += '<li><a href="work.html"><i class="icon-work"></i>Work</a></li>'
+html += '<li><a href="member.html"><i class="icon-member"></i>Member</a></li>'
 html += '<li><a href="computer.html"><i class="icon-computer"></i>Computer</a></li>'
 html += '<li><a href="link.html"><i class="icon-link"></i>Link</a></li>'
-html += '</ul>';
-html += '<ul id="subMenu">';
-html += '<li><a href="https://ocumcr.hatenablog.com/" target=""><i class="icon-blog"></i>Blog</a></li>';
-html += '<li><a href="https://x.com/ocu_mcr" target="_balank"><i class="icon-twitter"></i>Twitter</a></li>';
-html += '<li><a href="mailto:ocumcr2014@gmail.com"><i class="icon-mail"></i>Mail</a></li>';
-html += '</ul>';
-html += '</div>';
+html += "</ul>"
+html += '<ul id="subMenu">'
+html += '<li><a href="https://ocumcr.hatenablog.com/" target=""><i class="icon-blog"></i>Blog</a></li>'
+html += '<li><a href="https://x.com/ocu_mcr" target="_balank"><i class="icon-twitter"></i>Twitter</a></li>'
+html += '<li><a href="mailto:ocumcr2014@gmail.com"><i class="icon-mail"></i>Mail</a></li>'
+html += "</ul>"
+html += "</div>"
 
-document.write(html);
+document.write(html)
