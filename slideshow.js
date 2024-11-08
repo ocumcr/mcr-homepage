86e776e97a3cfe501a79ed5ded0d038e28a0b03b
IpotaZero <kiratoki512810@gmail.com>
2024-11-08 15:53:44 +0900

    index

---
+++ slideshow.js
@@ -0,0 +1,33 @@
+/**
+ * 画像スライドショーを表示するためのもの
+ */
+
+let slideIndex = 1
+
+function changeSlide(n) {
+    showSlides((slideIndex += n))
+}
+
+function currentSlide(n) {
+    showSlides((slideIndex = n))
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide")
+    const dots = document.getElementsByClassName("dot")
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+
+    slides[slideIndex - 1].style.display = "block"
+    dots[slideIndex - 1].className += " active"
+}
