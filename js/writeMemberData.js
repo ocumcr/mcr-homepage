b16f3dac3cfc5cf80106ded333b3e2a81adb700d
IpotaZero <kiratoki512810@gmail.com>
2024-11-27 23:41:36 +0900

    work obog

--- js/writeMemberData.js
+++ js/writeMemberData.js
@@ -31,6 +31,44 @@ const writeMemberData = async (grade) => {
     })
 }
 
+const writeOb = async (term) => {
+    const student = document.getElementById(`student-ob-og`)
+
+    const members = await safeLoadCsvAsObjects(`memberdata/student-${term}.csv`)
+
+    members.forEach((member) => {
+        student.innerHTML += `
+            <tr>
+                <th class="name">${member.name}</th>
+                <td class="course">${member.course}</td>
+                <td class="detail">
+                    趣味: ${member.hobby}<br>
+                    一言: ${member.comment}
+                </td>
+            </tr>
+        `
+    })
+}
+
+const writeMoreBefore = async () => {
+    const student = document.getElementById(`student-ob-og`)
+
+    const members = await safeLoadCsvAsObjects(`memberdata/student-それ以前.csv`)
+
+    members.forEach((member) => {
+        student.innerHTML += `
+            <tr>
+                <th class="name">${member.name}</th>
+                <td class="course">${member.course}</td>
+                <td class="detail">
+                    趣味: ${member.hobby}<br>
+                    一言: ${member.comment}
+                </td>
+            </tr>
+        `
+    })
+}
+
 const getFiscalYear = () => {
     const today = new Date()
     const year = today.getFullYear()
@@ -38,4 +76,27 @@ const getFiscalYear = () => {
 
     // 日本の年度（4月〜翌年3月）
     return month >= 4 ? year : year - 1
+}
+
+const writeAllMemberData = async () => {
+    for (let i = 0; i < 4; i++) {
+        await writeMemberData(i + 1)
+    }
+
+    document.getElementById("members").innerHTML += `
+    <table summary="サークルのメンバーの紹介" class="intro-table">
+        <caption>
+        OB-OG
+        </caption>
+        <tbody id="student-ob-og">
+        </tbody>
+    </table>
+  `
+
+    for (let i = 0; i < getFiscalYear() - 2024; i++) {
+        console.log(getFiscalYear() - 2024 - i)
+        await writeOb(getFiscalYear() - 2024 - i)
+    }
+
+    await writeMoreBefore()
 }
